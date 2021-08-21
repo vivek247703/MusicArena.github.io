baf2c786e82e5854405ccc155b5d9c0244728017
Vivek Hariyan <60876356+vivek247703@users.noreply.github.com>
2021-08-21 17:23:18 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,28 @@
+let allMusic = [
+ 
+    {
+        name: "Sairat",
+        artist: "Shreya Ghoshal",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Bagh Milkha Bagh",
+        artist: "unknown",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Zingat",
+        artist: "Ajay Atul",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Devak Kalji Re",
+        artist: "Ajay Atul",
+        img: "music-4",
+        src: "music-4"
+    },
+    
+    ];
